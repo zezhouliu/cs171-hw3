e78d9ff1addf3935ea4da5bb625f8ebcb6547a39
Alex Liu <zezhouliu@college.harvard.edu>
2014-03-14 11:24:42 -0400

    Finished adding nodes to difference graph

--- Problem2/differencegraph.js
+++ Problem2/differencegraph.js
@@ -30,6 +30,7 @@
             transform: "translate(" + margin.left + "," + margin.top + ")"
         });
 
+    var color = d3.scale.category10();
 
     d3.csv("timeline.csv", function(data) {
 
@@ -43,126 +44,125 @@
             return parseInt(s.replace(/,/g, ""), 10);
         };
 
-        data.forEach(function (d, i) {
-            d.USCensus = convertToInt(d.USCensus);
-            d.PopulationBureau = convertToInt(d.PopulationBureau);
-            d.UN = convertToInt(d.UN);
-            d.HYDE = convertToInt(d.HYDE);
-            d.Maddison = convertToInt(d.Maddison);
+        color.domain(d3.keys(data[0]).filter(function (key) { return key !== "year"; }));
+
+        var estimates = color.domain().map(function (name) {
+            return {
+                name: name,
+                values: data.map(function (d) {
+                    return { year: d.year, value: +d[name], estimated:0};
+                })
+            };
         });
 
-        var min_year = d3.min(data, function (d) { return d.year; });
-        var max_year = d3.max(data, function (d) { return d.year; });
+        estimates.forEach(function (d, i) {
+
+            d.years = [];
+            d.estimate = [];
+
+            d.values.map(function (d1, i1) {
+                if (d1.value) {
+                    d.years.push(d1.year);
+                    d.estimate.push(d1.value);
+                }
+            });
+
+            var year_extent = d3.extent(d.years);
+            var interpolate = d3.scale.linear().domain(year_extent).range(d.estimate);
+
+            d.values.forEach(function (d1, i1) {
+
+                if (d1.year < year_extent[1] && d1.year > year_extent[0] && d1.value  == 0) {
+                    d1.value = interpolate(d1.year);
+                }
+
+                d.values = d.values.filter(function (d1, i1) {
+                    return d1.value != 0;
+                });
+
+            });
+
+        });
 
         var xAxis, xScale, yAxis, yScale;
 
         // define the scale and axis for x
-        xScale = d3.scale.linear().domain([min_year, max_year]).range([0, bbVis.w]);  
+        xScale = d3.scale.linear().domain(d3.extent(data, function (d, i) { return d.year; })).range([0, bbVis.w]);
         xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(5);
 
         var min_value = 0;
-
-        // filter out the values for each category
-        var data_USCensus = data.filter(function (d, i) { return d.USCensus; });
-        var data_populationBureau = data.filter(function (d, i) { return d.PopulationBureau; });
-        var data_UN = data.filter(function (d, i) { return d.UN; });
-        var data_HYDE = data.filter(function (d, i) { return d.HYDE; });
-        var data_Maddison = data.filter(function (d, i) { return d.Maddison; });
-
-        // get the max of each category
-        var extent_census = d3.extent(data_USCensus, function (d) { return d.USCensus; });
-        var extent_populationbureau = d3.extent(data_populationBureau, function (d) { return d.PopulationBureau; });
-        var extent_UN = d3.extent(data_UN, function (d) { return d.UN; });
-        var extent_HYDE = d3.extent(data_HYDE, function (d) { return d.HYDE; });
-        var extent_Maddison = d3.extent(data_Maddison, function (d) { return d.Maddison; });
-
-        console.log(extent_census);
-        console.log(extent_populationbureau);
-        console.log(extent_UN);
-        console.log(extent_HYDE);
-        console.log(extent_Maddison);
-
-        var max_value = Math.max(extent_census[1], extent_populationbureau[1], extent_UN[1], extent_HYDE[1], extent_Maddison[1]);
+        var max_value = d3.max(estimates, function (c) { return d3.max(c.values, function (v) { return v.value; }); });
 
         // define the scale and axis for y
         yScale = d3.scale.linear().domain([max_value, min_value]).range([0, height]);
         yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(7);
 
-        var color = d3.scale.category10();
-        var datasets = [];
-        datasets.push(data_USCensus, data_populationBureau, data_UN, data_HYDE, data_Maddison);
-        var extents = [];
-        extents.push(extent_census, extent_populationbureau, extent_UN, extent_HYDE, extent_Maddison);
-
-        var keys = ["USCensus", "PopulationBureau", "UN", "HYDE", "Maddison"];
-
-        // interpolate for each dataset
-        var interpolate = function (i) {
-            
-            var year_extent = d3.extent(datasets[i], function (d) { return d.year; });
-            var estimate = d3.scale.linear().domain(year_extent).range(extents[i]);
-
-            data.forEach(function (d1, i1) {
-                if (d1.year > year_extent[0] && d1.year < year_extent[1] && datasets[i].indexOf(d1) == -1) {
-                    d1[keys[i]] = estimate(d1.year);
-                    datasets[i].push(d1);
-                }
-            })
-        };
-
-        for (var i = 0; i < datasets.length; ++i) {
-            interpolate(i);
-        }
-
-        var consensusline = d3.svg.line().interpolate("basis")
-                        .x(function (d, i) {
-                            console.log(d.year);
-                            return xScale(d.year);
-                        })
-                        .y(function (d, i) {
-                            return yScale(d.USCensus);
-                        });
-
-
-        svg.selectAll(".line")
-            .data(data_USCensus)
-            .enter()
-            .append("path")
-            .attr("class", "line")
-            .style("stroke", color(i))
-            .attr("d", consensusline);
-        
-
-        //var all_datapoints = d3.merge(datasets);
-        //svg.selectAll("dot")
-        //.data(all_datapoints)
-        //.enter().append("circle")
-        //.attr("r", 1.5)
-        //.attr("cx", function (d) { return xScale(d.year); })
-        //.attr("cy", function (d) { return yScale(d.value); })
-        //.attr("stroke", function (d, i) {
-        //    return color(d.type);
-        //})
-        //.attr("fill", function (d, i) {
-        //    return color(d.type);
-        //});
-
 
         // Add the X Axis
-		svg.append("g")
+        svg.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0," + height + ")")
             .call(xAxis);
 
         // Add the Y Axis
-		svg.append("g")
+        svg.append("g")
             .attr("class", "y axis")
             .call(yAxis);
 
-		  
-		//visFrame.append("rect");
+        var line = d3.svg.line()
+            .interpolate("basis")
+            .x(function (d) { return xScale(d.year); })
+            .y(function (d) { return yScale(d.value); });
 
-//        // add y axis to svg !
+        var estimate = svg.selectAll(".estimate")
+            .data(estimates)
+            .enter()
+            .append("g")
+            .attr("class", "estimate");
+
+        estimate.append("path")
+            .attr("class", "line")
+            .attr("d", function (d) { return line(d.values); })
+            .style("stroke", function (d) { return color(d.name); });
+
+        var point = estimate.append("g")
+            .attr("class", "line-point");
+
+        point.selectAll("circle")
+            .data(function (d) { return d.values; })
+            .enter()
+            .append("svg:circle")
+            .attr("cx", function (d) { return xScale(d.year); })
+            .attr("cy", function (d) { return yScale(d.value); })
+            .attr("year", function (d) { return d.year; })
+            .attr("r", 2)
+            .attr("fill", function (d) { return color(this.parentNode.__data__.name); })
+            .attr("stroke", function (d) { return color(this.parentNode.__data__.name);});
+
+
+        var legend = svg.selectAll(".legend")
+            .data(color.domain())
+            .enter().append("g")
+            .attr("class", "legend")
+           .attr("transform", function (d, i) { return "translate(0," + i * 20 + ")"; });
+
+        legend.append("rect")
+            .attr("x", width - 18)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", color);
+
+        legend.append("text")
+            .attr("x", width - 24)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .attr("font-size", "10px")
+            .style("text-anchor", "end")
+            .text(function (d) { return d; });
+
+        var std_dev = function (d, i) {
+            
+        }
 
 
     };
